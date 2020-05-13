ed011b0803412e630ce2d2fdf61137bdca3c4e9a
Nick Saunders <nick@saunde.rs>
2020-05-12 19:01:05 -0700

    Validation logic

--- index.js
+++ index.js
@@ -1,26 +1,68 @@
-var composePlugins = function(a,b) {
-  return [
-    function(decls) {
-      return getFn(a)(getFn(b)(decls));
-    },
-    []
-      .concat(a[1] || [])
-      .concat(b[1] || [])
-      .reduce(function(xs, x) {
-        return xs.indexOf(x) === -1 ? xs.concat(x) : xs;
-      }, [])
-  ];
-  
-  function getFn(x) {
-    if (typeof x === "object") {
-      return x[0];
+var clone = function (x) {
+  return JSON.parse(JSON.stringify(x));
+};
+
+var validDecls = function (x, fallback) {
+  if (!x) {
+    return fallback;
+  }
+
+  if (typeof x !== "object") {
+    return fallback;
+  }
+
+  if (x instanceof Array) {
+    return fallback;
+  }
+
+  for (var key in x) {
+    switch (typeof x[key]) {
+      case "string":
+      case "number":
+      case "boolean":
+        continue;
+      default:
+        return fallback;
     }
+  }
+
+  return x;
+};
+
+var getFn = function (x) {
+  if (typeof x[0] === "function") {
+    return x[0];
+  }
+  if (typeof x === "function") {
+    return x;
+  }
+  return function (x) {
     return x;
   };
 };
 
-module.exports = function() {
-  var result = function(x) { return x; };
+var composePlugins = function (a, b) {
+  return [
+    function (decls) {
+      var resb = validDecls(getFn(b)(clone(decls)), decls);
+      var resa = validDecls(getFn(a)(clone(resb)), resb);
+      return resa;
+    },
+    []
+      .concat(a[1] || [])
+      .concat(b[1] || [])
+      .reduce(function (xs, x) {
+        return xs.indexOf(x) === -1 && typeof x === "string"
+          ? xs.concat(x)
+          : xs;
+      }, []),
+  ];
+};
+
+module.exports = function () {
+  var result = function (x) {
+    return x;
+  };
   for (var i = arguments.length - 1; i >= 0; i--) {
     result = composePlugins(arguments[i], result);
   }
--- test/index.js
+++ test/index.js
@@ -5,11 +5,11 @@ describe("compose function", () => {
   it("combines properties associated with each plugin", () => {
     const expected = ["margin-x", "margin-y", "padding-x", "padding-y"];
 
-    const [ _, actual ] = compose(
+    const [_, actual] = compose(
       x => x,
-      [ x => x, ["margin-x"] ],
+      [x => x, ["margin-x"]],
       x => x,
-      [ x => x, ["margin-y", "padding-x", "padding-y"] ],
+      [x => x, ["margin-y", "padding-x", "padding-y"]],
     );
 
     expect(actual).to.deep.equal(expected);
@@ -17,12 +17,12 @@ describe("compose function", () => {
 
   it("composes functions from right to left", () => {
     const add = n => decls => {
-      Object.keys(decls).forEach(key => decls[key] = decls[key] + n);
+      Object.keys(decls).forEach(key => (decls[key] = decls[key] + n));
       return decls;
     };
 
     const mult = n => decls => {
-      Object.keys(decls).forEach(key => decls[key] = decls[key] * n);
+      Object.keys(decls).forEach(key => (decls[key] = decls[key] * n));
       return decls;
     };
 
@@ -39,12 +39,46 @@ describe("compose function", () => {
     expect(actual).to.deep.equal(expected);
   });
 
-  it("should deduplicate recognized properties", () => {
+  it("does not mutate declarations", () => {
+    const add = n => decls => {
+      Object.keys(decls).forEach(key => (decls[key] = decls[key] + n));
+      return decls;
+    };
+
+    const decls = { foo: 1 };
+    const result = compose(add(1), add(2), add(3))[0](decls);
+
+    expect(decls.foo).to.equal(1);
+    expect(result.foo).to.equal(7);
+  });
+
+  it("bypasses plugins that don't return declaration objects", () => {
+    const plugins = [
+      decls => decls,
+      decls => null,
+      decls => {
+        delete decls["color"];
+        return decls;
+      },
+    ];
+
+    const actual = compose.apply(null, plugins)[0]({
+      background: "white",
+      color: "black",
+    });
+    expect(actual).to.deep.equal({ background: "white" });
+  });
+
+  it("deduplicates recognized properties", () => {
     const [_, properties] = compose(
-      [ x => x, ["padding-x"] ],
-      [ x => x, ["padding-x"] ],
+      [x => x, ["padding-x"]],
+      [x => x, ["padding-x"]],
     );
-    console.log(properties);
     expect(properties).to.deep.equal(["padding-x"]);
+  });
+
+  it("removes non-string properties", () => {
+    const actual = compose([x => x, ["foo", 1]], [x => x, ["bar", false]])[1];
+    expect(actual).to.deep.equal(["foo", "bar"]);
   });
 });
